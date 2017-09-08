0098a3ff6f1faaf66b631741c12ba87ea7e2e87d
Carlos Gomez Cainzos <carlos.gomezcainzos@gmail.com>
2017-09-08 03:01:13 +0200

    [WIP] event does not work yet

--- mesos_operator_stream/main.go
+++ mesos_operator_stream/main.go
@@ -4,23 +4,31 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"encoding/base64"
 
 	"github.com/gogo/protobuf/jsonpb"
 	"github.com/gogo/protobuf/proto"
 	"github.com/ondrej-smola/mesos-go-http/lib/client/leader"
 	"github.com/ondrej-smola/mesos-go-http/lib/operator/master"
 	"github.com/ondrej-smola/mesos-go-http/lib/client"
-	"encoding/base64"
+	"github.com/go-kit/kit/log"
+
+
 )
 
 type config struct {
 	endpoints []string
 	printResponse func(proto.Message)
 	ctx context.Context
-	credentials []string
+	credentials string
 }
 
 func main() {
+
+
+	w := log.NewSyncWriter(os.Stderr)
+	logger := log.NewLogfmtLogger(w)
+
 	cfg := &config{
 		printResponse: jsonpbPrint,
 		ctx:           context.Background(),
@@ -28,35 +36,51 @@ func main() {
 		credentials:   base64.StdEncoding.EncodeToString([]byte("open:DrE6o5r67qnf38H9K5KgGhoeRHqwU6sVNBVStHqw")),
 	}
 
-	leader.New(cfg.endpoints,
-		leader.WithClientOpts(client.WithRecordIOFraming()),
-	)
+	getLeader := func(opts ...leader.Opt) *master.Client {
+		return master.New(leader.New(
+			cfg.endpoints,
+			leader.WithLogger(logger),
+			leader.WithClientOpts(
+				client.WithRequestOpts(
+					client.WithAuthorization("Basic " + cfg.credentials)))),
+		)
+	}
 
-	leader.WithClientOpts(client.WithAuthorization(cfg.credentials))
-	leader.Opt(client.WithAuthorization(cfg.credentials))
+	v, err := getLeader().GetVersion(cfg.ctx)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	cfg.printResponse(v)
 
 	events := master.NewEventStream(
-		leader.New(cfg.endpoints,
+		leader.New(
+			cfg.endpoints,
 			leader.WithClientOpts(client.WithRecordIOFraming()),
+			leader.WithLogger(logger),
+			leader.WithClientOpts(
+				client.WithRequestOpts(
+					client.WithAuthorization("Basic " + cfg.credentials))),
 			),
 		cfg.ctx,
 	)
 
+	fmt.Printf("Received events: %d\n", len(events))
 	for ev := range events {
-				fmt.Println(ev)
-				if ev.Err != nil {
-					fmt.Println(ev.Err)
-					os.Exit(1)
-				} else {
-					cfg.printResponse(ev.Event)
-				}
-
-			}
-
+		fmt.Println("Pasas por loop de eventos!!")
+		fmt.Println(ev)
+		if ev.Err != nil {
+			fmt.Println(ev.Err)
+			os.Exit(1)
+		} else {
+			cfg.printResponse(ev.Event)
+		}
+	}
+	fmt.Println("Fuera de loop de eventos")
 }
 
 func jsonpbPrint(m proto.Message) {
-		marsh := jsonpb.Marshaler{EmitDefaults: true}
+	marsh := jsonpb.Marshaler{EmitDefaults: true}
 	b, err := marsh.MarshalToString(m)
 	if err != nil {
 		fmt.Println(err)
